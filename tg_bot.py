0c6e3662f9f83dee988d0a2d19da1dfa93c15668
serdroi <145987898+serdroi@users.noreply.github.com>
2023-09-25 14:28:10 +0200

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -1,2 +1,5 @@
 import telebot;
 bot = telebot.TeleBot('%token%');
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+  @bot.message_handler(content_types=['text', 'document', 'audio'])
